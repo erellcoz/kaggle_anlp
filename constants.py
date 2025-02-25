33822d330c4b97fdbdf94630e60dd29235eb6a81
Eucalypso <hugo.khlaut@student-cs.fr>
2025-02-25 09:02:47 +0100

    fix(analyse_data): add alphabet column

--- constants.py
+++ constants.py
@@ -8,6 +8,7 @@ TEST_DATA_PATH = "data/test_without_labels.csv"
 LABEL_COLUMN = "Label"
 TEXT_COLUMN = "Text"
 EMBEDDING_COLUMN = "Embedding"
+ALPHABET_COLUMN = "Alphabet"
 
 # EMBEDDING
 EMBEDDING_SIZE = 512
@@ -16,16 +17,18 @@ EMBEDDING_SIZE = 512
 # Définition des plages Unicode pour différents alphabets
 ALPHABETS = {
     "Latin": (0x0041, 0x007A),
-    "Cyrillique": (0x0400, 0x04FF),
-    "Arabe": (0x0600, 0x06FF),
-    "Hébreu": (0x0590, 0x05FF),
-    "Chinois": (0x4E00, 0x9FFF),
+    "Cyrillic": (0x0400, 0x04FF),
+    "Arabic": (0x0600, 0x06FF),
+    "Hebrew": (0x0590, 0x05FF),
+    "Chinese": (0x4E00, 0x9FFF),
     "Hiragana": (0x3040, 0x309F),
-    "Katana": (0x30A0, 0x30FF),
-    "Coréen": (0xAC00, 0xD7AF),
-    "Grec": (0x0370, 0x03FF),
+    "Katakana": (0x30A0, 0x30FF),
+    "Korean": (0xAC00, 0xD7AF),
+    "Greek": (0x0370, 0x03FF),
     "Gujarati": (0x0A80, 0x0AFF),
     "Devanagari": (0x0900, 0x097F),
-    "Thaï": (0x0E00, 0x0E7F),
-    "Géorgien": (0x10A0, 0x10FF),
+    "Thai": (0x0E00, 0x0E7F),
+    "Georgian": (0x10A0, 0x10FF),
 }
+TINY_ALPHABETS = ["Hiragana", "Katakana", "Gujarati", "Korean", "Thai"]
+SMALL_ALPHABETS = ["Hebrew", "Greek", "Georgian"]
